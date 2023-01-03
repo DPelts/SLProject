bdd91b995e18ad69c789232a7083ac44e464be84
DPelts <dmytriy.p@gmail.com>
2023-01-03 08:11:10 +0100

    Mouth implementation

--- modules/sl/source/mesh/SLFaceAnim.cpp
+++ modules/sl/source/mesh/SLFaceAnim.cpp
@@ -1,5 +1,6 @@
 #include <SLMesh.h>
-#include "SLSphere.h"
+// #include "SLSphere.h"
+#include <unordered_map>
 #include "SLFaceAnim.h"
 
 #include <prebuilt/win64_mediapipe/include/face_mesh_api.h>
@@ -7,9 +8,9 @@
 #define LMK(Index, XYZ) _pFacialLandmarks[Index * 3 + XYZ]
 #define LMK3D(index) LMK(index, 0), LMK(index, 1), LMK(index, 2)
 #define CLAMP_TOP(value) \
-    if (value < 0.0f) { value = 0.0f; }
-#define CLAMP_BOTTOM(value) \
     if (value > 1.0f) { value = 1.0f; }
+#define CLAMP_BOTTOM(value) \
+    if (value < 0.0f) { value = 0.0f; }
 #define CLAMP(value) \
     if (value < 0.0f) { value = 0.0f; } \
     if (value > 1.0f) {value = 1.0f; }
@@ -17,19 +18,6 @@
 SLFaceMesh::SLFaceMesh(SLAssetManager* assetMgr, const SLstring& name) : SLMesh(assetMgr, name)
 {
     
-}
-
-void SLFaceMesh::startCapture()
-{
-    mediapipeStatus = FACE_MESH_INIT();
-
-    int size = FACE_MESH_GET_FACE_LANDMARKS_SIZE() / 3;
-    idleFaceLandmarks.resize(size);
-    maxFaceStretch.resize(size);
-    minFaceStretch.resize(size);
-    _pFacialLandmarks = FACE_MESH_GET_FACE_LANDMARKS();
-
-    isCaptureStarted = true;
 }
 
 SLVec3f getCenterPoint(SLVec3f p1, SLVec3f p2)
@@ -42,8 +30,204 @@ SLfloat getLengthAtScale(SLVec3f p1, SLVec3f p2, SLfloat scale)
     return ((p2 - p1).length()) / scale;
 }
 
-SLfloat minEye = 10.0f;
-SLfloat maxEye = 0.0f;
+void SLFaceMesh::initMaps()
+{
+    _localFLMap.reserve(FLISIZE);
+    _rotatedFLMap.reserve(FLISIZE);
+
+    for (SLint i = 0; i < FLISIZE; i++)
+    {
+        const SLint index = _usedLandmarkIndicesList[i];
+        _localFLMap[index]   = SLVec3f::ZERO;
+        _rotatedFLMap[index] = SLVec3f::ZERO;
+    }
+}
+
+void SLFaceMesh::updateLocalFLMap(const SLVec3f pivot, const SLfloat pivotLength)
+{
+    for (SLint i = 0; i < FLISIZE; i++)
+    {
+        const SLint index = _usedLandmarkIndicesList[i];
+        _localFLMap[index] = (SLVec3f(LMK3D(index)) - pivot) / pivotLength;
+    }
+}
+
+void SLFaceMesh::updateRotatedLocalFLMap()
+{
+    SLVec3f forward = _localFLMap[FLI::nose];  // Nose
+    SLVec3f right   = getCenterPoint(_localFLMap[FLI::face_Side_R_Upper], _localFLMap[FLI::face_Side_R_Lower]);
+    SLVec3f normal  = forward.normalized();
+    SLVec3f tangent  = right.normalized();
+    SLVec3f down;
+    down.cross(normal, tangent);
+    down            = -down;
+    SLVec3f binormal = down.normalized();
+    // std::cout << normal << "\t" << tangent << "\t" << binormal << std::endl;
+    // SLMat4f matrix = SLMat4f(tangent.x, tangent.y, tangent.z, 0.0f, normal.x, normal.y, normal.z, 0.0f, binormal.x, binormal.y, binormal.z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    SLfloat r00               = tangent.x;
+    SLfloat r01               = tangent.y;
+    SLfloat r02               = tangent.z;
+    SLfloat r12               = normal.z;
+    SLfloat r22               = binormal.z; 
+
+    SLfloat roll            = atan(-r12 / r22);
+    SLfloat pitch           = atan(r02 * cos(roll) / r22);
+    SLfloat yaw            = atan(-r01 / r00);
+    
+    roll *= 180.0f / PI;
+    pitch *= 180.0f / PI;
+    yaw *= 180.0f / PI;
+
+    if (roll > 0)
+    {
+        roll = -(90.0f - roll);
+    }
+    else if (roll < 0)
+    {
+        roll = roll + 90.0f;
+        pitch = -pitch;
+    }
+
+    _headRot                   = SLVec3f(roll, pitch, yaw);
+
+    for (SLint i = 0; i < FLISIZE; i++)
+    {
+        SLint index = _usedLandmarkIndicesList[i];
+        SLVec3f point = _localFLMap[index];
+        SLfloat theta = roll * 0.0174533f;
+        // SLMat3f r     = SLMat3f(1.0f, 0.0f, 0.0f, 0.0f, cos(theta), -sin(theta), 0.0f, sin(theta), cos(theta));
+        SLMat3f r            = SLMat3f(1.0f, 0.0f, 0.0f, 0.0f, cos(theta), sin(theta), 0.0f, -sin(theta), cos(theta));
+        point = r * point;
+        theta = pitch * 0.0174533f;
+        // r                    = SLMat3f(cos(theta), 0.0f, sin(theta), 0.0f, 1.0f, 0.0f, -sin(theta), 0.0f, cos(theta));
+        r                    = SLMat3f(cos(theta),  0.0f, -sin(theta), 0.0f, 1.0f, 0.0f, sin(theta), 0.0f, cos(theta));
+        point = r * point;
+        theta = yaw * 0.0174533 / 2.5f;
+        // r                    = SLMat3f(cos(theta), -sin(theta), 0.0f, sin(theta), cos(theta), 0.0f, 0.0f, 0.0f, 1.0f);
+        r                    = SLMat3f(cos(theta), sin(theta), 0.0f, -sin(theta),  cos(theta),  0.0f, 0.0f, 0.0f, 1.0f);
+        point = r * point;
+        _rotatedFLMap[index] = point;
+    }
+}
+
+void SLFaceMesh::startCapture()
+{
+    mediapipeStatus = FACE_MESH_INIT();
+
+    int size = FACE_MESH_GET_FACE_LANDMARKS_SIZE() / 3;
+    idleFaceLandmarks.resize(size);
+    maxFaceStretch.resize(size);
+    minFaceStretch.resize(size);
+    _pFacialLandmarks = FACE_MESH_GET_FACE_LANDMARKS();
+
+    initMaps();
+
+    isCaptureStarted = true;
+}
+
+void SLFaceMesh::eyeBlinkTracking()
+{
+    SLVec3f face_Eye_L_Upper = _localFLMap[FLI::eye_Lid_L_Upper]; // (SLVec3f(LMK3D(386)) - pivot) / pivotLength;
+    SLVec3f face_Eye_L_Lower = _localFLMap[FLI::eye_Lid_L_Lower]; // (SLVec3f(LMK3D(374)) - pivot) / pivotLength;
+    SLVec3f face_Eye_R_Upper = _localFLMap[FLI::eye_Lid_R_Upper]; // (SLVec3f(LMK3D(159)) - pivot) / pivotLength;
+    SLVec3f face_Eye_R_Lower = _localFLMap[FLI::eye_Lid_R_Lower]; // (SLVec3f(LMK3D(145)) - pivot) / pivotLength;
+
+    SLfloat eye_L_scale = (face_Eye_L_Upper - face_Eye_L_Lower).length();
+    SLfloat eye_L_minus = eye_L_scale - 0.016f;
+    CLAMP_BOTTOM(eye_L_minus)
+    SLfloat ajusted_eye_L_scale = 1.0f - (eye_L_minus) / 0.025f;
+    CLAMP(ajusted_eye_L_scale)
+
+    SLfloat eye_R_scale = (face_Eye_R_Upper - face_Eye_R_Lower).length();
+    SLfloat eye_R_minus = eye_R_scale - 0.016f;
+    CLAMP_BOTTOM(eye_R_minus)
+    SLfloat ajusted_eye_R_scale = 1.0f - (eye_R_minus) / 0.025f;
+    CLAMP(ajusted_eye_R_scale)
+
+    bsTime[BlendShapeIndex::eyeBlinkLeft] = ajusted_eye_L_scale;
+    transformSkinWithBlendShapes(BlendShapeIndex::eyeBlinkLeft);
+    bsTime[BlendShapeIndex::eyeBlinkRight] = ajusted_eye_R_scale;
+    transformSkinWithBlendShapes(BlendShapeIndex::eyeBlinkRight);
+}
+
+void SLFaceMesh::jawTracking()
+{
+    // SLVec3f eyePivot = getCenterPoint(_rotatedFLMap[FLI::eye_Outer_L], _rotatedFLMap[FLI::eye_Outer_L]);
+    // SLVec3f jaw       = _rotatedFLMap[FLI::jaw];
+    SLVec3f eyePivot = getCenterPoint(_localFLMap[FLI::eye_Outer_L], _localFLMap[FLI::eye_Outer_R]);
+    SLVec3f jaw       = _localFLMap[FLI::jaw];
+    // SLVec3f diff            = jaw - eyePivot;
+    SLfloat diff            = (jaw - eyePivot).length();
+    _jawHeight           = (diff - 0.36f) / 0.08f; // 0.42f;
+    CLAMP_TOP(_jawHeight)
+    CLAMP_BOTTOM(_jawHeight)
+    // std::cout << diff << "\t" << _jawHeight << std::endl;
+    bsTime[BlendShapeIndex::jawOpen] = _jawHeight;
+    transformSkinWithBlendShapes(BlendShapeIndex::jawOpen);
+}
+
+void SLFaceMesh::mouthTracking()
+{
+    SLfloat lipDist = (_localFLMap[FLI::mouth_UpperLip_Outer] - _localFLMap[FLI::mouth_LowerLip_Outer]).length();
+    // Mouth open/close
+    SLfloat       jawPercent         = (_jawHeight - 0.05f) / 0.7f;
+    SLfloat       mouthHeightPercent = (lipDist) / 0.176f;
+    CLAMP_BOTTOM(mouthHeightPercent)
+    if (jawPercent < 0.1f)
+        jawPercent = 0.1f;
+    SLfloat       ratio              = mouthHeightPercent / jawPercent;
+    if (_jawHeight > 0.7f)
+        ratio = 1.0f;
+    SLfloat regulator = 1.0f - ratio;
+    // std::cout << "Jaw: " << _jawHeight << "\tRegu: " << regulator << "\tLipDist: " << lipDist << "\t jawP:" << jawPercent << "\t mouthP:" << mouthHeightPercent << "\t Ratio:" << ratio << std::endl;
+    regulator                           = (regulator > _jawHeight) ? _jawHeight : regulator;
+    CLAMP_TOP(regulator)
+    CLAMP_BOTTOM(regulator)
+    bsTime[BlendShapeIndex::mouthClose] = regulator;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthClose);
+
+    // Mouth Pucker
+    SLfloat mouthCornerDist = (_localFLMap[FLI::mouth_Corner_L] - _localFLMap[FLI::mouth_Corner_R]).length();
+    SLfloat puckerRatio = 1.0 - (mouthCornerDist - 0.06f) / 0.05f;
+    CLAMP_TOP(puckerRatio)
+    CLAMP_BOTTOM(puckerRatio)
+    bsTime[BlendShapeIndex::mouthPucker] = puckerRatio;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthPucker);
+
+    // Mouth Corner Movement
+    SLVec3f mouthCornerCenter = getCenterPoint(_localFLMap[FLI::mouth_Corner_L], _localFLMap[FLI::mouth_Corner_R]);
+    SLVec3f mouthLipCenter    = getCenterPoint(_localFLMap[FLI::mouth_UpperLip_Outer], _localFLMap[FLI::mouth_LowerLip_Outer]);
+    SLfloat centerDist        = (mouthCornerCenter - mouthLipCenter).length();
+    // SLfloat cornerDist        = (_localFLMap[FLI::mouth_Corner_L] - _localFLMap[FLI::mouth_Corner_R]).length();
+    // std::cout << mouthCornerDist << "\t" << centerDist << std::endl;
+    SLfloat topRatio = (centerDist - 0.039f) / 0.01f;
+    CLAMP_TOP(topRatio)
+    CLAMP_BOTTOM(topRatio)
+    SLfloat cornerRatio = (mouthCornerDist - 0.115) / 0.015f;
+    CLAMP_TOP(cornerRatio)
+    CLAMP_BOTTOM(cornerRatio)
+    
+    SLfloat smilePercent = cornerRatio * topRatio;
+    SLfloat dimpelPercent = cornerRatio - smilePercent;
+    bsTime[BlendShapeIndex::mouthSmileLeft] = smilePercent;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthSmileLeft);
+    bsTime[BlendShapeIndex::mouthSmileRight] = smilePercent;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthSmileRight);
+    bsTime[BlendShapeIndex::mouthDimpleLeft] = dimpelPercent;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthDimpleLeft);
+    bsTime[BlendShapeIndex::mouthDimpleRight] = dimpelPercent;
+    transformSkinWithBlendShapes(BlendShapeIndex::mouthDimpleRight);
+    std::cout << "topRatio: " << topRatio << "\t cornerRatio: " << cornerRatio << "\t smile: " << smilePercent << "\t dimpel: " << dimpelPercent << std::endl;
+    // SLVec3f mouthCornerCenterRotated = getCenterPoint(_rotatedFLMap[FLI::mouth_Corner_L], _rotatedFLMap[FLI::mouth_Corner_R]);
+    // SLVec3f mouthLipCenterRotated    = getCenterPoint(_rotatedFLMap[FLI::mouth_UpperLip_Outer], _rotatedFLMap[FLI::mouth_LowerLip_Outer]);
+    // SLfloat centerDistRotated        = (mouthCornerCenter.y - mouthLipCenter.y);
+    // std::cout << centerDistRotated << std::endl;
+    // SLVec3f eyePivotRotated = getCenterPoint(_rotatedFLMap[FLI::eye_Outer_L], _rotatedFLMap[FLI::eye_Outer_R]);
+    // eyePivotRotated.y += mouthLipCenter.y - eyePivotRotated.y;
+    // SLfloat mouthCenterOffset = eyePivotRotated.x - mouthLipCenterRotated.x;
+    // std::cout << mouthCenterOffset << std::endl;
+
+}
 
 void SLFaceMesh::draw(SLSceneView* sv, SLNode* node)
 {
@@ -63,29 +247,13 @@ void SLFaceMesh::draw(SLSceneView* sv, SLNode* node)
             SLVec3f left  = getCenterPoint(face_Side_Left_Upper, face_Side_Left_Lower);
             SLVec3f pivot = getCenterPoint(right, left);
             SLfloat pivotLength = (right - left).length() * 3;
-            std::cout << pivotLength << std::endl;
 
-            SLVec3f face_Eye_L_Upper = (SLVec3f(LMK3D(386)) - pivot) / pivotLength;
-            SLVec3f face_Eye_L_Lower = (SLVec3f(LMK3D(374)) - pivot) / pivotLength;
-            SLVec3f face_Eye_R_Upper = (SLVec3f(LMK3D(159)) - pivot) / pivotLength;
-            SLVec3f face_Eye_R_Lower = (SLVec3f(LMK3D(145)) - pivot) / pivotLength;
+            updateLocalFLMap(pivot, pivotLength);
+            updateRotatedLocalFLMap();
 
-            SLfloat eye_L_scale = (face_Eye_L_Upper - face_Eye_L_Lower).length();
-            SLfloat eye_L_minus         = eye_L_scale - 0.016f;
-            CLAMP_BOTTOM(eye_L_minus)
-            SLfloat ajusted_eye_L_scale = 1.0f - (eye_L_minus) / 0.025f;
-            CLAMP(ajusted_eye_L_scale)
-
-            SLfloat eye_R_scale         = (face_Eye_R_Upper - face_Eye_R_Lower).length();
-            SLfloat eye_R_minus = eye_R_scale - 0.016f;
-            CLAMP_BOTTOM(eye_R_minus)
-            SLfloat ajusted_eye_R_scale = 1.0f - (eye_R_minus) / 0.025f;
-            CLAMP(ajusted_eye_R_scale)
-
-            bsTime[BlendShapeIndex::eyeBlinkLeft] = ajusted_eye_L_scale;
-            transformSkinWithBlendShapes(BlendShapeIndex::eyeBlinkLeft);
-            bsTime[BlendShapeIndex::eyeBlinkRight] = ajusted_eye_R_scale;
-            transformSkinWithBlendShapes(BlendShapeIndex::eyeBlinkRight);
+            eyeBlinkTracking();
+            jawTracking();
+            mouthTracking();
 
 
 
--- modules/sl/source/mesh/SLFaceAnim.h
+++ modules/sl/source/mesh/SLFaceAnim.h
@@ -24,129 +24,124 @@ public:
 private:
     enum BlendShapeIndex
     {
-        eyeBlinkLeft = 0,
-        eyeLookDownLeft = 1,
-        eyeLookInLeft = 2,
-        eyeLookOutLeft = 3,
-        eyeLookUpLeft = 4,
-        eyeSquintLeft = 5,
-        eyeWideLeft = 6,
-        eyeBlinkRight = 7,
-        eyeLookDownRight = 8,
-        eyeLookInRight = 9,
-        eyeLookOutRight = 10,
-        eyeLookUpRight = 11,
-        eyeSquintRight = 12,
-        eyeWideRight = 13,
-        jawForward = 14,
-        jawLeft = 15,
-        jawRight = 16,
-        jawOpen = 17,
-        mouthClose = 18,
-        mouthFunnel = 19,
-        mouthPucker = 20,
-        mouthRight = 21,
-        mouthLeft = 22,
-        mouthSmileLeft = 23,
-        mouthSmileRight = 24,
-        mouthFrownRight = 25,
-        mouthFrownLeft = 26,
-        mouthDimpleLeft = 27,
-        mouthDimpleRight = 28,
-        mouthStretchLeft = 29,
-        mouthStretchRight = 30,
-        mouthRollLower = 31,
-        mouthRollUpper = 32,
-        mouthShrugLower = 33,
-        mouthShrugUpper = 34,
-        mouthPressLeft = 35,
-        mouthPressRight = 36,
-        mouthLowerDownLeft = 37,
+        eyeBlinkLeft        = 0,
+        eyeLookDownLeft     = 1,
+        eyeLookInLeft       = 2,
+        eyeLookOutLeft      = 3,
+        eyeLookUpLeft       = 4,
+        eyeSquintLeft       = 5,
+        eyeWideLeft         = 6,
+        eyeBlinkRight       = 7,
+        eyeLookDownRight    = 8,
+        eyeLookInRight      = 9,
+        eyeLookOutRight     = 10,
+        eyeLookUpRight      = 11,
+        eyeSquintRight      = 12,
+        eyeWideRight        = 13,
+        jawForward          = 14,
+        jawLeft             = 15,
+        jawRight            = 16,
+        jawOpen             = 17,
+        mouthClose          = 18,
+        mouthFunnel         = 19,
+        mouthPucker         = 20,
+        mouthRight          = 21,
+        mouthLeft           = 22,
+        mouthSmileLeft      = 23,
+        mouthSmileRight     = 24,
+        mouthFrownRight     = 25,
+        mouthFrownLeft      = 26,
+        mouthDimpleLeft     = 27,
+        mouthDimpleRight    = 28,
+        mouthStretchLeft    = 29,
+        mouthStretchRight   = 30,
+        mouthRollLower      = 31,
+        mouthRollUpper      = 32,
+        mouthShrugLower     = 33,
+        mouthShrugUpper     = 34,
+        mouthPressLeft      = 35,
+        mouthPressRight     = 36,
+        mouthLowerDownLeft  = 37,
         mouthLowerDownRight = 38,
-        mouthUpperUpLeft = 39,
-        mouthUpperUpRight = 40,
-        browDownLeft = 41,
-        browDownRight = 42,
-        browInnerUp = 43,
-        browOuterUpLeft = 44,
-        browOuterUpRight = 45,
-        cheekPuff = 46,
-        cheekSquintLeft = 47,
-        cheekSquintRight = 48,
-        noseSneerLeft = 49,
-        noseSneerRight = 50,
-        tongueOut = 51,
+        mouthUpperUpLeft    = 39,
+        mouthUpperUpRight   = 40,
+        browDownLeft        = 41,
+        browDownRight       = 42,
+        browInnerUp         = 43,
+        browOuterUpLeft     = 44,
+        browOuterUpRight    = 45,
+        cheekPuff           = 46,
+        cheekSquintLeft     = 47,
+        cheekSquintRight    = 48,
+        noseSneerLeft       = 49,
+        noseSneerRight      = 50,
+        tongueOut           = 51,
     };
 
-    enum FaceLandmarkIndex
+    enum FLI // FaceLandmarkIndex
     {
-        eyeTopLeft          = 385,
-        eyeTopRight         = 158,
-        jaw                 = 151,
-        // eyeBottomLeft       = 2,
-        // eyeBottomRight      = 3,
-        // eyeLookUpLeft       = 4,
-        // eyeSquintLeft       = 5,
-        // eyeWideLeft         = 6,
-        // eyeBlinkRight       = 7,
-        // eyeLookDownRight    = 8,
-        // eyeLookInRight      = 9,
-        // eyeLookOutRight     = 10,
-        // eyeLookUpRight      = 11,
-        // eyeSquintRight      = 12,
-        // eyeWideRight        = 13,
-        // jawForward          = 14,
-        // jawLeft             = 15,
-        // jawRight            = 16,
-        // jawOpen             = 17,
-        // mouthClose          = 18,
-        // mouthFunnel         = 19,
-        // mouthPucker         = 20,
-        // mouthRight          = 21,
-        // mouthLeft           = 22,
-        // mouthSmileLeft      = 23,
-        // mouthSmileRight     = 24,
-        // mouthFrownRight     = 25,
-        // mouthFrownLeft      = 26,
-        // mouthDimpleLeft     = 27,
-        // mouthDimpleRight    = 28,
-        // mouthStretchLeft    = 29,
-        // mouthStretchRight   = 30,
-        // mouthRollLower      = 31,
-        // mouthRollUpper      = 32,
-        // mouthShrugLower     = 33,
-        // mouthShrugUpper     = 34,
-        // mouthPressLeft      = 35,
-        // mouthPressRight     = 36,
-        // mouthLowerDownLeft  = 37,
-        // mouthLowerDownRight = 38,
-        // mouthUpperUpLeft    = 39,
-        // mouthUpperUpRight   = 40,
-        // browDownLeft        = 41,
-        // browDownRight       = 42,
-        // browInnerUp         = 43,
-        // browOuterUpLeft     = 44,
-        // browOuterUpRight    = 45,
-        // cheekPuff           = 46,
-        // cheekSquintLeft     = 47,
-        // cheekSquintRight    = 48,
-        // noseSneerLeft       = 49,
-        // noseSneerRight      = 50,
-        // tongueOut           = 51,
+        eye_Lid_L_Upper   = 386,
+        eye_Lid_L_Lower   = 374,
+        eye_Lid_R_Upper   = 159,
+        eye_Lid_R_Lower   = 145,
+        eye_Outer_L       = 33,
+        eye_Outer_R       = 263,
+        face_Side_R_Upper = 447,
+        face_Side_R_Lower = 366,
+        face_Side_L_Upper = 137,
+        face_Side_L_Lower = 227,
+        nose              = 5,
+        jaw               = 152,
+        mouth_UpperLip_Outer = 0,
+        mouth_LowerLip_Outer = 17,
+        mouth_Corner_L       = 291,
+        mouth_Corner_R       = 61,
     };
 
-    SLbool    isCaptureStarted = false;
+    SLbool    isCaptureStarted          = false;
     SLbool    hasFirstFrameBeenRecorded = false;
-    const int originIndex = 0;
-    float* _pFacialLandmarks;
-    int    mediapipeStatus = 0;
-    SLVVec3f idleFaceLandmarks;
-    SLVVec3f maxFaceStretch;
-    SLVVec3f minFaceStretch;
+    const int originIndex               = 0;
+    float*    _pFacialLandmarks;
+    int       mediapipeStatus = 0;
+    SLVVec3f  idleFaceLandmarks;
+    SLVVec3f  maxFaceStretch;
+    SLVVec3f  minFaceStretch;
 
 private:
-    // SLVMesh meshList;
+    #define FLISIZE 14
+    SLint _usedLandmarkIndicesList[FLISIZE]
+    {
+      FLI::eye_Lid_L_Upper,
+      FLI::eye_Lid_L_Lower,
+      FLI::eye_Lid_R_Upper,
+      FLI::eye_Lid_R_Lower,
+      FLI::face_Side_R_Upper,
+      FLI::face_Side_R_Lower,
+      FLI::face_Side_L_Upper,
+      FLI::face_Side_L_Lower,
+      FLI::nose,
+      FLI::jaw,
+      FLI::mouth_UpperLip_Outer,
+      FLI::mouth_LowerLip_Outer,
+      FLI::mouth_Corner_L,
+      FLI::mouth_Corner_R,
+    };
+
+    std::unordered_map<SLint, SLVec3f> _localFLMap;
+    std::unordered_map<SLint, SLVec3f> _rotatedFLMap;
+    SLVec3f                            _pivot = SLVec3f::ZERO;
+    SLVec3f                            _headRot = SLVec3f::ZERO;
+    SLfloat                            _jawHeight = 0.0f;
+
+    void SLFaceMesh::initMaps();
+    void SLFaceMesh::updateLocalFLMap(const SLVec3f, const SLfloat);
+    void SLFaceMesh::updateRotatedLocalFLMap();
+
+    void eyeBlinkTracking();
+    void jawTracking();
+    void mouthTracking();
+    void irisTracking();
+    void browTracking();
+    void cheekSquintTracking();
 };
-
-
 #endif
